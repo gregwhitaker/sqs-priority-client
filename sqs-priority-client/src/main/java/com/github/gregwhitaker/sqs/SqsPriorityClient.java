60209030a077d9108afc5f1a2f54afaad02f1810
gregwhitaker <greg.whitaker@nike.com>
2021-04-12 16:02:45 -0700

    Doing work

--- sqs-priority-client/src/main/java/com/github/gregwhitaker/sqs/SqsPriorityClient.java
+++ sqs-priority-client/src/main/java/com/github/gregwhitaker/sqs/SqsPriorityClient.java
@@ -22,6 +22,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.SqsAsyncClientBuilder;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
