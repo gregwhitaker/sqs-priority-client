a4e32be09e03d49efdcfd7b80553e76626c21c17
gregwhitaker <greg.whitaker@nike.com>
2021-04-12 15:42:08 -0700

    Doing work

--- sqs-priority-client/src/integTest/java/com/github/gregwhitaker/sqs/ReceiveMessageIntegTest.java
+++ sqs-priority-client/src/integTest/java/com/github/gregwhitaker/sqs/ReceiveMessageIntegTest.java
@@ -15,38 +15,6 @@
  */
 package com.github.gregwhitaker.sqs;
 
-import software.amazon.awssdk.services.sqs.SqsClient;
-
-import java.net.URI;
-import java.util.concurrent.CountDownLatch;
-
 public class ReceiveMessageIntegTest {
-  private static final String SQS_ENDPOINT = "http://localhost:4566";
 
-  public static void main(String... args) throws Exception {
-    final SqsClient sqs = SqsClient.builder()
-            .endpointOverride(URI.create(SQS_ENDPOINT))
-            .build();
-
-    final SqsPriorityClient sqsPriorityClient = SqsPriorityClient.builder(sqs)
-            .withQueues()
-              .queue("high-priority-queue", 0.80)
-              .queue("medium-priority-queue", 0.15)
-              .queue("low-priority-queue", 0.05)
-            .end()
-            .withMaxNumberOfMessages(10)
-            .build();
-
-    final CountDownLatch latch = new CountDownLatch(1);
-
-    sqsPriorityClient.receiveMessages()
-            .flatMap(message -> {
-              System.out.printf("Message %s: %s%n", message.messageId(), message.body());
-              return sqsPriorityClient.deleteMessage(message.receiptHandle());
-            })
-            .doOnComplete(latch::countDown)
-            .subscribe();
-
-    latch.await();
-  }
 }
---
+++ sqs-priority-client/src/test/java/com/github/gregwhitaker/sqs/SqsPriorityClientConfigTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2021 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.gregwhitaker.sqs;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SqsPriorityClientConfigTest {
+
+  @Test
+  public void defaultMaxNumberOfMessagesShouldBe10() {
+    // Given
+    SqsPriorityClientConfig config = new SqsPriorityClientConfig();
+
+    // Then
+    assertEquals(10, config.getMaxNumberOfMessages());
+  }
+
+  @Test
+  public void defaultMaxEmptyReceiveCountShouldBe10() {
+    // Given
+    SqsPriorityClientConfig config = new SqsPriorityClientConfig();
+
+    // Then
+    assertEquals(10, config.getMaxEmptyReceiveCount());
+  }
+
+  @Test
+  public void defaultEmptyReceiveTimeoutShouldBe10Seconds() {
+    // Given
+    SqsPriorityClientConfig config = new SqsPriorityClientConfig();
+
+    // Then
+    assertEquals(Duration.ofSeconds(10), config.getEmptyReceiveTimeout());
+  }
+
+  @Test
+  public void shouldDefaultWeightedQueuesToEmptyIfNoneSpecified() {
+    // Given
+    SqsPriorityClientConfig config = new SqsPriorityClientConfig();
+
+    // Then
+    assertTrue(config.getWeightedQueues().isEmpty());
+  }
+}
